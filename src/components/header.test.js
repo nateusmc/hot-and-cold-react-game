e0da5a5b5f680a336a16b2505fc446fca09cf488
Nate Garrett <nate.garrett1992@gmail.com>
2017-11-10 13:51:08 -0600

    added tests for header

---
+++ src/components/header.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import '../tests-config'
+import Header from './header';
+
+describe('<Header />', () => {
+    it('Renders without crashing', () => {
+        shallow(<Header />);
+    });
+
+    it('Hides the info modal initially', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.find('InfoModal').exists()).toEqual(false);
+    });
+
+    it('Should render the info modal when toggled', () => {
+        const wrapper = shallow(<Header />);
+        wrapper.instance().toggleInfoModal(true);
+        wrapper.update();
+        expect(wrapper.find('InfoModal').exists()).toEqual(true);
+    });
+});
